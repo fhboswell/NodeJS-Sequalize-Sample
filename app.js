d829dfcbffcd0d57263c0ba81b48a6f82a0cc278
fhboswell <franklin.henry.boswell@gmail.com>
2017-10-19 18:35:07 -0700

    convert to ejs

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var expressLayouts = require('express-ejs-layouts');
+
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -8,14 +10,19 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 
+
+
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(expressLayouts);
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
